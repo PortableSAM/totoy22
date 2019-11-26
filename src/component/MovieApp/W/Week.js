b37fbac92214dd07e0eee16c4e53d90467f22293
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-26 19:12:50 +0900

    modify WeeklyMovie List

--- src/component/MovieApp/W/Week.js
+++ src/component/MovieApp/W/Week.js
@@ -2,6 +2,7 @@ import React from "react";
 import Axios from "axios";
 
 import { MainURL, Key, TargetWt } from "../lib";
+import WeekList from "./WeekList";
 console.log(TargetWt);
 export default class WeeklyMovie extends React.Component {
   state = {
@@ -14,7 +15,15 @@ export default class WeeklyMovie extends React.Component {
     const url = `${MainURL}/searchWeeklyBoxOfficeList.json?key=${Key}&targetDt=${TargetWt}`;
     await Axios.get(url)
       .then(res => res.data)
-      .then(data => console.log(data));
+      .then(data => {
+        this.setState({
+          weekly: data.boxOfficeResult,
+          wList: data.boxOfficeResult.weeklyBoxOfficeList,
+          isLoading: false
+        });
+        console.log(this.state.weekly);
+        console.log(this.state.wList);
+      });
   };
 
   componentDidMount() {
@@ -22,6 +31,46 @@ export default class WeeklyMovie extends React.Component {
   }
 
   render() {
-    return <div></div>;
+    const { weekly, wList, isLoading } = this.state;
+    if (wList.length > 0) {
+      return (
+        <div>
+          {isLoading ? (
+            <div className="loader">
+              <span className="loader_text">
+                <h2>Data Loading</h2>
+              </span>
+            </div>
+          ) : (
+            <div>
+              <div>
+                <h3>{weekly.boxofficeType}</h3>
+                <h5>{weekly.showRange}</h5>
+              </div>
+              <div className="wMovies">
+                {wList.map((weeklyMovie, index) => (
+                  <WeekList
+                    id={weeklyMovie.id}
+                    key={index}
+                    title={weeklyMovie.movieNm}
+                    open={weeklyMovie.openDt}
+                    rank={weeklyMovie.rank}
+                    people={weeklyMovie.audiAcc}
+                    point={weeklyMovie.showCnt}
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h3>{weekly.boxofficeType}</h3>
+          <h3>Gathering data</h3>
+        </div>
+      );
+    }
   }
 }
