fd86c5578fb0f5d5d758438d4556515b36d4e8f1
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-26 11:20:31 +0900

    modify JS files

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React from "react";
-
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { BrowserRouter as Router, Link, Route } from "react-router-dom";
 import "./App.css";
+import Home from "./component/Home";
+import Project from "./component/Project";
 
-function App() {
-  return (
-    <div>
-      <h2>Hello React</h2>
-    </div>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <Router>
+        <div>
+          <Layout>
+            <Header>
+              <Navigation>
+                <Link to="/">Home</Link>
+                <Link to="/about">About</Link>
+                <Link to="/project">Project</Link>
+              </Navigation>
+            </Header>
+          </Layout>
+        </div>
+      </Router>
+    );
+  }
 }
-
-export default App;
