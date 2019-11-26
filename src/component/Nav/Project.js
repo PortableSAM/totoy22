c32f33eb521b5bbcfacf31669013f38a56a6e8bf
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-11-26 19:34:26 +0900

    Add React-MDL to Project.js

--- src/component/Nav/Project.js
+++ src/component/Nav/Project.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Tabs, Tab, Grid, Cell } from "react-mdl";
+
 import Daily from "../MovieApp/D/Daily";
 import Weekly from "../MovieApp/W/Week";
 
@@ -7,11 +9,38 @@ export default class Project extends React.Component {
     super(props);
     this.state = { activeTab: 1 };
   }
+  toggleCategori() {
+    if (this.state.activeTab === 1) {
+      return (
+        <div>
+          <Daily />
+        </div>
+      );
+    } else if (this.state.activeTab === 0) {
+      return (
+        <div>
+          <Weekly />
+        </div>
+      );
+    }
+  }
+
   render() {
     return (
-      <div>
-        <Daily />
-        <Weekly />
+      <div className="categori_tabs">
+        <Tabs
+          activeTab={this.state.activeTab}
+          onChange={tabId => this.setState({ activeTab: tabId })}
+          ripple
+        >
+          <Tab>Weekly</Tab>
+          <Tab>Daily</Tab>
+        </Tabs>
+        <Grid>
+          <Cell col={12}>
+            <div className="project_content">{this.toggleCategori()}</div>
+          </Cell>
+        </Grid>
       </div>
     );
   }
