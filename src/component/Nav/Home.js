a2206dea4ba78079ec88a85a6b78b18ba72e3342
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-17 20:38:55 +0900

    add lib

--- src/component/Nav/Home.js
+++ src/component/Nav/Home.js
@@ -2,11 +2,13 @@ import React from "react";
 
 import "./Home.css";
 import Clock from "../Clock/Clock";
+import Weather from "../Location/Weather";
 
 export default class Home extends React.Component {
   render() {
     return (
       <div className="home_text">
+        <Weather />
         <h2>This is Home</h2>
         <Clock />
       </div>
