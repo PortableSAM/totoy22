924629bb884186a853dc99ef19cef6d7f2f9b7f4
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-19 16:54:52 +0900

    Add Weather Icon to Weather.js

--- src/component/Weather/Weather.js
+++ src/component/Weather/Weather.js
@@ -1,15 +1,17 @@
 import React from "react";
 import "./Weather.css";
+import { Grid, Cell } from "react-mdl";
 
 const App_Id = "be70cc0b47a6331dc61023d7137347cb";
 
-export default class TESTOWA20191218 extends React.Component {
+export default class Weather extends React.Component {
   state = {
     data: "",
     temp: 0,
     condition: "",
     location: "",
-    discript: ""
+    discript: "",
+    icon: null
   };
 
   componentDidMount() {
@@ -36,27 +38,39 @@ export default class TESTOWA20191218 extends React.Component {
           temp: json.main.temp,
           condition: json.weather[0].main,
           location: json.name,
-          discript: json.weather[0].description
+          discript: json.weather[0].description,
+          icon: json.weather[0].icon
         });
         console.log(this.state);
       });
   };
 
   render() {
-    const { temp, condition, location, discript } = this.state;
+    const { temp, condition, location, discript, icon } = this.state;
     return (
       <div className="weather_container">
         <div className="weather_title">
           <h5>Current Weather</h5>
         </div>
-        <div className="weather_loca">
-          <p>장소: {location}</p>
-        </div>
-        <div className="weather_condition">
-          <p>
-            기상: {condition}, {discript}
-          </p>
-          <p>온도: {temp}℃</p>
+        <div
+          className="weather_condition"
+          style={{ width: "100%", margin: "auto" }}
+        >
+          <Grid>
+            <Cell className="weather_left" col={8}>
+              <p>장소: {location}</p>
+              <p>
+                기상: {condition}, {discript}
+              </p>
+              <p>온도: {temp}℃</p>
+            </Cell>
+            <Cell className="weather_right" col={4}>
+              <img
+                src={`http://openweathermap.org/img/w/${icon}.png`}
+                alt="wthr_img"
+              />
+            </Cell>
+          </Grid>
         </div>
       </div>
     );
