6b32b653d3b7e325a9d162805f4264f13643d0cf
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-20 16:56:00 +0900

    TestNoteApp complete

--- src/test component/TestTodo/TestNote.js
+++ src/test component/TestTodo/TestNote.js
@@ -7,11 +7,22 @@ export default class TestNote extends Component {
     super(props);
     this.noteContent = props.noteContent;
     this.noteId = props.noteId;
+    this.handleRemoveNote = this.handleRemoveNote.bind(this);
+  }
+
+  handleRemoveNote(id) {
+    this.props.removeNote(id);
   }
 
   render(props) {
     return (
       <div className="note fade-in">
+        <span
+          className="closebtn"
+          onClick={() => this.handleRemoveNote(this.noteId)}
+        >
+          <i className="fas fa-eraser" />
+        </span>
         <p className="noteContent">{this.noteContent}</p>
       </div>
     );
--- src/test component/TestTodo/TestNoteApp.js
+++ src/test component/TestTodo/TestNoteApp.js
@@ -1,17 +1,63 @@
 import React from "react";
 import TestNote from "./TestNote";
 import "./TestNoteApp.css";
+import TestNoteForm from "../TestTodoForm/TestNoteForm";
+import { DB_Config } from "../config/config";
+import firebase from "firebase/app";
+import "firebase/database";
 
 export default class TestNoteApp extends React.Component {
   constructor(props) {
     super(props);
+    this.addNote = this.addNote.bind(this);
+    this.removeNote = this.removeNote.bind(this);
+
+    this.app = firebase.initializeApp(DB_Config);
+    this.database = this.app
+      .database()
+      .ref()
+      .child("notes");
+
+    // We're going to setup the React state of our component
     this.state = {
-      notes: [
-        { id: 1, noteContent: "Note 1" },
-        { id: 2, noteContent: "Note 2" }
-      ]
+      notes: []
     };
   }
+
+  componentDidMount() {
+    const previousNotes = this.state.notes;
+
+    // DatabaseSnapshot
+    this.database.on("child_added", snap => {
+      previousNotes.push({
+        id: snap.key,
+        noteContent: snap.val().noteContent
+      });
+      this.setState({
+        notes: previousNotes
+      });
+    });
+
+    this.database.on("child_removed", snap => {
+      for (let i = 0; i < previousNotes.length; i++) {
+        if (previousNotes[i].id === snap.key) {
+          previousNotes.splice(i, 1);
+        }
+      }
+      this.setState({
+        notes: previousNotes
+      });
+    });
+  }
+
+  addNote(note) {
+    this.database.push().set({ noteContent: note });
+  }
+
+  removeNote(noteId) {
+    this.database.child(noteId).remove();
+  }
+
   render() {
     return (
       <div className="notesWrapper">
@@ -25,11 +71,14 @@ export default class TestNoteApp extends React.Component {
                 noteContent={note.noteContent}
                 noteId={note.id}
                 key={note.id}
+                removeNote={this.removeNote}
               />
             );
           })}
         </div>
-        <div className="notesFooter">Footer is here!!!</div>
+        <div className="notesFooter">
+          <TestNoteForm addNote={this.addNote} />
+        </div>
       </div>
     );
   }
