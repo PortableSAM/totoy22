bb1f522c942fbfa6311789d567a0277532792173
Lee-h-saem <53569830+Lee-h-saem@users.noreply.github.com>
2019-12-20 17:18:41 +0900

    firebase deploy

--- src/test component/config/config.js
+++ src/test component/config/config.js
@@ -1,10 +1,11 @@
 // Your web app's Firebase configuration
 export const DB_Config = {
-  apiKey: "AIzaSyBF5PN_8NWCVL-sL7HV3dIAix9BrRJ_zps",
-  authDomain: "tododatabase-dc5dc.firebaseapp.com",
-  databaseURL: "https://tododatabase-dc5dc.firebaseio.com",
-  projectId: "tododatabase-dc5dc",
-  storageBucket: "tododatabase-dc5dc.appspot.com",
-  messagingSenderId: "407237445957",
-  appId: "1:407237445957:web:a5ca748c2fa5cf91cd162c"
+  apiKey: "##################################",
+  authDomain: "##########.firebaseapp.com",
+  databaseURL: "###############",
+  projectId: "##############",
+  storageBucket: "##############.appspot.com",
+  messagingSenderId: "#################",
+  appId: "%:$$$$$$:!!!!:**************************",
+  measurementId: "G-&&&&&&&&&&&&"
 };
